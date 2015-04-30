30dbdcea7dc62620250a9f80e5a0c46470d6b9cd
yannickoo <github@yannick.md>
2015-04-30 16:02:29 +0200

    uses location hash for allow sharing filter result

--- js/app.js
+++ js/app.js
@@ -18,6 +18,31 @@
             selection.removeAllRanges();
             selection.addRange(range);
           }
+        },
+        hashChange = function() {
+          var value = location.hash.substr(1),
+              unicodeWrapper = document.querySelector('.unicodes'),
+              figures = unicodeWrapper.querySelectorAll('figure'),
+              matches = [].filter.call(figures, function(figure) {
+                return figure.innerText.toLowerCase().indexOf(value) !== -1;
+              });
+
+          search.value = value;
+
+          if (matches.length) {
+            unicodeWrapper.classList.add('filtered');
+
+            for (var i = 0; i < figures.length; i++) {
+              figures[i].classList.remove('show');
+            }
+
+            matches.forEach(function(match) {
+              match.classList.add('show');
+            });
+          }
+          else {
+            unicodeWrapper.classList.add('hide');
+          }
         };
 
     for (var i = 0; i < symbols.length; i++) {
@@ -27,29 +52,10 @@
     }
 
     search.addEventListener('input', function() {
-      var value = this.value,
-          unicodeWrapper = document.querySelector('.unicodes'),
-          figures = unicodeWrapper.querySelectorAll('figure'),
-          matches = [].filter.call(figures, function(figure) {
-            return figure.innerText.toLowerCase().indexOf(value) !== -1;
-          });
-
-          console.log(matches);
-
-      if (matches.length) {
-        unicodeWrapper.classList.add('filtered');
-
-        for (var i = 0; i < figures.length; i++) {
-          figures[i].classList.remove('show');
-        }
-
-        matches.forEach(function(match) {
-          match.classList.add('show');
-        });
-      }
-      else {
-        unicodeWrapper.classList.add('hide');
-      }
+      location.hash = this.value;
     });
+
+    window.addEventListener('hashchange', hashChange);
+    hashChange();
   });
 })();
